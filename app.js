a99224feca8494072bd37790cc7bfa948fdfff80
Sharjeel Hassan <mohammadsharjeelhassan@gmail.com>
2024-01-19 19:46:58 +0500

    working on strings

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Chapter 1 (Alerts) 172021252829 
+/*// Chapter 1 (Alerts) 172021 q8 252829 
 
 // 1. Alert these following (individually):  
 // I. First Name 
@@ -694,25 +694,49 @@ console.log(ui.toUpperCase());
 
 // 7.  var cityName = “kaRacHi”; Convert the string represented by a cityName in Capitalisation is the writing of a word with its first letter in uppercase and the remaining letters in lowercase. 
 
-
+*/
 // Chapter 22 - 25 (Strings) 
 
 // 1.  "captain" has been assigned to variable “sameWords”. You want to slice "ap" out of it. 
+let sameWords = "captain";
+let sameWordAP = sameWords.slice(1,3);
+console.log(sameWordAP);
+
+
 
 // 2.  The number of characters in the string will be assigned to the variable. 
 
+let charL = "Islamic Republic of Pakistan";
+let charLengths = charL.length;
+console.log(charLengths);
+
 // 3.  The string "elephant" has been assigned to the variable animal.Slice the four middle characters out of the string and assign it to the variable seg, which hasn't been declared beforehand. 
 
-// 4.  Find the number of characters in the string represented by a variable and assign the number to a second variable. 
+let animal ="elephant";
+let seg = animal.slice(2, 6);
+console.log(seg);
 
+// 4.  Find the number of characters in the string represented by a variable and assign the number to a second variable. 
+let charLen = "Islamic Republic of Pakistan";
+let charLength = charLen.length;
+console.log(charLength);
 
 // 5.  In a first statement measure how many characters there are in a string represented by a variable. In a second statement slice all but the first character and last 3 characters of the string and assign it to a second variable that hasn't been declared beforehand. 
-
+let stringChar = "Jawan Pakistan karimabad";
+let panga = stringChar.slice(1,stringChar.length - 3);
+console.log(panga);
 
 // 6.  var text = "To be or not to be."; var indx = text.indexOf("be");  What is the value of indx? 
+var text = "To be or not to be.";
+var indx = text.indexOf("be");
+console.log(indx);
 
 // 7.  var text = "To be or not to be."; var indx = text.lastIndexOf("be"); What is the value of indx? 
 // Note: Try the above both examples by yourself. 
+
+var text = "To be or not to be.";
+var indx = text.lastIndexOf("be");
+console.log(indx);
 
 
 // 8.  Find the index of the first character of the last instance of "go" in  the string represented by the variable text and assign the number  to the variable indx, which hasn't been declared beforehand. 
@@ -722,19 +746,40 @@ console.log(ui.toUpperCase());
 // 9.  Code the first line of an if statement that tests whether a segment with an index represented by indexNum exists in a string. 
 
 //10. In this string "abcde", what character is at index 2? (Use charAt) 
-
+let chart = "abcde";
+console.log(chart.charAt(2));
 // 11. Find the 10th character in the string represented by text and assign it to the variable cha, which hasn't been declared beforehand. 
 
-// 12. Find the last character in the string represented by str and assign it to x, which hasn't been declared beforehand. 
+text ="To be or not to be.";
+console.log(text.charAt(10));
 
+// 12. Find the last character in the string represented by str and assign it to x, which hasn't been declared beforehand. 
+text ="To be or not to be.";
+console.log(text.charAt(text.length-1));
+
+console.log(text[text.length-1]);
 
 // 13. Find the the 5th character in a string represented by input and assign it to cha, which hasn't been declared beforehand. 
-
+text ="To be&or not to be.";
+let cha;
+console.log(cha=text.charAt(5));
+console.log(cha);
 
 // 14. Code the first line of an if statement that tests whether the  3rd character of a string represented by a variable is a particular character. 
+let stringCharacter = "PakistanZindabad";
+if(stringCharacter.charAt(3) == "2") {console.log("i found you")}
+else{console.log("recheck again please"
+);}
 
 
 // 15. Code a for loop that cycles through all the characters of a  string represented by a variable and assigns each character to an element of an array that has been declared beforehand. In the string represented by reply replace the first instance of "no" with "yes" and assign the revised string to revisedReply, which hasn't been declared beforehand. 
+
+stringCharacter = "Pakistan sa zinda bhag";
+let arrStringChar =stringCharacter.split('');
+console.log(arrStringChar);
+
+
+// for (i=0; i<stringCharacter.length; i++)
 
 
 // 16. In a string represented by str replace the first instance of "1" with "one" and assign the revised string to newStr, which hasn't been declared beforehand. 
@@ -847,6 +892,8 @@ var day = d.getDay();
 console.log(day);
 
 // 4.  The day has been extracted from the Date object and assigned to d. The names of the days of the week have been assigned to the array dayNames. Alert the current day with array index. 
+
+
 
 // 5.  Extract all parts of the Date and Time and assign it to the variable  which has not been declared beforehand. 
 
